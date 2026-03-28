42cc74ee634d7288bf6408729cab9d8b592b9326
NovEclipse <89341484+NovEclipse@users.noreply.github.com>
2026-03-28 12:44:17 +0000

    Add files via upload
    Added the obfuscation script.

---
+++ the file OOFer.py
@@ -0,0 +1,28 @@
+hexmap = {
+    '0': '\u200B', '1': '\u200C', '2': '\u200D', '3': '\u200E',
+    '4': '\u200F', '5': '\uFEFF', '6': '\u00AD', '7': '\u200A',
+    '8': '\u2009', '9': '\u00A0', 'a': '\u180E', 'b': '\u2060',
+    'c': '\u2064', 'd': '\u2065', 'e': '\u2066', 'f': '\u2067',
+}
+sep = '\u2068'
+
+try:
+    with open("SampleFile.txt", "r", encoding = "latin-1") as infile:
+        with open("SampleFile.oof", "w", encoding = "utf-8") as outfile:
+            while True:
+                char = infile.read(1)
+                if not char:
+                    break
+                if char == "\n":
+                    outfile.write("\n")
+            
+                else:
+                    hexed = format(ord(char), '02x')
+                    outfile.write(hexmap[hexed[0]])
+                    outfile.write(hexmap[hexed[1]])
+                    outfile.write(sep)
+        
+    print("encode done.")
+
+except Exception as e:
+    print(f"Error occurred: {e}")
